eee48df1b0e9301cf05e5d885b751864c07d75ff
kiran <kiran.ganesh9@gmail.com>
2024-12-23 18:20:42 -0600

    Adding main.py file

---
+++ main.py
@@ -0,0 +1,33 @@
+print("Hello")
+
+print(2+3)
+
+print('kiran')
+
+print('kiran\'b')
+
+#variables
+x = 10
+y = 20
+_ = 30
+
+#print(x+y)
+
+print(3+2)
+
+print(_+y)
+
+num = [1, 2, 3, 4]
+
+print(num)
+
+num.extend([1, 4])
+
+print(num)
+
+tup= (1, 2, 3, 3, 3, 4, 4)
+
+print(tup.count(4))
+
+import sys
+print(sys.executable)
